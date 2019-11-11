f727d288b4b5bd8f6fe00f9bbdabcd30e110f5f0
Constantino Saldana <constantino.saldana@gmail.com>
2019-11-11 18:05:59 +0000

    logo pizzaz

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,7 +8,7 @@ const ImageLinkForm = () => {
                 {`This will detect faces in picture, try it or else... don't`}
             </p>
             <div className='center'>
-                <div className='form pa4 br3 shadow-5'>
+                <div className='form center pa4 br3 shadow-5'>
                     <input className='f2 pa2 w-70 center' type='text' />
                     <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
                 </div>
