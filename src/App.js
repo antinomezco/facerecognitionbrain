65e9bfd8a732e3755b2baac4cdd46633ee7c780e
Constantino Saldana <constantino.saldana@gmail.com>
2019-11-11 19:47:00 +0000

    ran

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
+const particleOptions = {
+    particles: {
+        number: {
+            value:30,
+            density: {
+                enable:true,
+                value_area: 800
+            }
+        }
+    }
+}
+
 function App() {
   return (
     <div className="App">
+        <Particles className='particles'
+              params={particlesOptions}
+        />
         <Navigation/>
         <Logo />
         <Rank />
