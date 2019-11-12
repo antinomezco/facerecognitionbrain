fa8836328a7f28a056a305fd7e28db837aa780a3
Constantino Saldana <constantino.saldana@gmail.com>
2019-11-12 19:49:43 +0000

    button

---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FaceRecognition = () => {
+    return(
+        <div>
+            <img src='https://samples.clarifai.com/face-det.jpg' />
+        </div>
+    )
+}
+
+export default FaceRecognition;
