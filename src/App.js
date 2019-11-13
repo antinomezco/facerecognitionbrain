5dc2a197a6ea0d12ef5fa376f76e03f6d274a9fd
Constantino Saldana <constantino.saldana@gmail.com>
2019-11-13 19:03:39 +0000

    imagen

--- src/App.js
+++ src/App.js
@@ -39,8 +39,17 @@ class App extends Component {
         const image = document.getElementById('inputImage');
         const width = Number(image.width);
         const height = Number(image.height);
-        console.log('width, height');
-        console.log(width, height);
+        return {
+            leftCol: clarifaiFace.left_col * width,
+            topRow: clarifaiFace.top_row * height,
+            rightCol: width - (clarifaiFace.right_col * width),
+            topRow: height - (clarifaiFace.bottom_row * height),//,
+        }
+    }
+
+    displayFaceBox (box) =>{
+        console.log(box);
+        this.setState({ box: box});
     }
 
     onInputChange = (event) =>{
@@ -50,7 +59,7 @@ class App extends Component {
     onButtonSubmit = () =>{
         this.setState({imageUrl: this.state.input})
         app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-        .then(response => this.calculateFaceLocation(response))
+        .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
         .catch(err => console.log(err))
     }
 
