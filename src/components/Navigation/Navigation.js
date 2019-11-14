4ff20d9370c1f41a20683ba2cbc5eb10aa0e7629
Constantino Saldana <constantino.saldana@gmail.com>
2019-11-14 20:25:09 +0000

    fix?

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navigation = ({onRouteChange, isSignedIn}) => {
     return(
-        if (ifSignedIn){
+        if (isSignedIn){
             return(
                 <nav style={{ display:'flex', justifyContent:'flex-end'}}>
                     <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
